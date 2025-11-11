e708c5803b75b84b595b471c5239c7d6a0b16f17
nyaivu <marvtfs@gmail.com>
2025-11-11 14:51:29 +0800

    Added Search Functionality

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -5,23 +5,26 @@ import { useState } from "react";
 import ProductItem from "./ProductItem";
 import { apiGetProducts } from "@/services/apiService"; // 1. Import from apiService
 import { PaginatedProductResponse } from "@/interfaces/api"; // 2. Import the correct response type
-import { useSessionStore } from "@/stores/sessionStore";
 
 const ProductsGrid = () => {
-  const { role } = useSessionStore();
-
   // 3. Add state for pagination
   const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
 
   // 4. Update useQuery to handle the new function and response type
-  const { data, isLoading, isError, error } = useQuery<
+  const { data, isLoading, isError, error, refetch } = useQuery<
     PaginatedProductResponse,
     Error
   >({
-    queryKey: ["products", page], // 5. Add page to queryKey
-    queryFn: () => apiGetProducts(page), // 6. Call the new paginated function
+    queryKey: ["products", page, searchQuery], // 5. Add page to queryKey
+    queryFn: () => apiGetProducts(page, searchQuery), // 6. Call the new paginated function
     placeholderData: keepPreviousData, // (Recommended) Keeps old data while new data loads
   });
+
+  function handleSearch(term: string) {
+    setSearchQuery(term);
+    refetch();
+  }
 
   if (isLoading) {
     return <p className="p-4 text-center">Loading products...</p>;
@@ -36,8 +39,22 @@ const ProductsGrid = () => {
   return (
     <section className="flex flex-col gap-4 p-4 md:">
       <div className="flex flex-row justify-between">
-        <h2 className="font-bold text-3xl">Products {role}</h2>
+        <h2 className="font-bold text-3xl">Products</h2>
       </div>
+
+      {/* Filter/search options */}
+      <div className="">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="search..."
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+        />
+      </div>
+
       <div className="flex flex-col items-start md:grid grid-cols-3 grid-flow-row gap-4">
         {/* Map over data.data (the array is nested) */}
         {data?.data?.map((product) => (
